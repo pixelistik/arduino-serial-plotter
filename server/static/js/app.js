dbad77266c62939e7fb93236d337b72d7c417244
Pixelistik <code@pixelistik.de>
2016-05-26 10:05:37 +0200

    Add simple epoch example
    From https://epochjs.github.io/epoch/getting-started/

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -9,3 +9,15 @@ eventSource.onmessage=  function(e) {
       targetContainer.appendChild(newElement);
 
 };
+
+
+var data = [
+  { label: 'Layer 1', values: [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2} ] },
+  { label: 'Layer 2', values: [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 4} ] }
+];
+
+var areaChartInstance = $('#area').epoch({
+    type: 'area',
+    data: data,
+    axes: ['left', 'right', 'bottom']
+});
