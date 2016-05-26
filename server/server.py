f8f136e69ffecbc36793bb0e26a49143e16b36ec
Pixelistik <code@pixelistik.de>
2016-05-26 08:58:19 +0200

    SSE example with Generator
    Based on https://medium.com/code-zen/python-generator-and-html-server-sent-events-3cdf14140e56#.lxp5y8afg

--- server/server.py
+++ server/server.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, Response
+from random import randint
+from time import sleep
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'hello world!'
 
+
+@app.route("/stream")
+def stream():
+    def eventStream():
+        while True:
+            result = randint(0, 99)
+            sleep(1)
+            #yield "data: %d\n\n" % result
+            yield "data: some%s\n\n" % result
+    return Response(eventStream(), mimetype="text/event-stream")
+
 def main():
+    app.debug = True
     app.run()
 
 if __name__ == "__main__":
